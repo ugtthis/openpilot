2ce3cef5587c6ae2a3ab7036a4cd66d7d46a2cc2
David Ratunuman <dratunuman@gmail.com>
2024-06-27 14:58:21 -0700

    Commented out all shadow related code

--- selfdrive/ui/qt/widgets/driver_alert_cluster.h
+++ selfdrive/ui/qt/widgets/driver_alert_cluster.h
@@ -36,10 +36,13 @@ private:
     QColor iconColor;
     QColor textColor;
     int borderWidth;
-    QColor shadowColor;
-    qreal shadowOpacity;
-    qreal shadowBlurRadius;
-    qreal shadowSpread;
+
+    // REMOVE - Shadow code
+    // QColor shadowColor;
+    // qreal shadowOpacity;
+    // qreal shadowBlurRadius;
+    // qreal shadowSpread;
+
     std::array<QColor, NUM_CIRCLES> circleColors;
   };
 
@@ -69,8 +72,10 @@ private:
   AlertProperties getAlertProperties(int alertLevel);
   void drawAlertBar(QPainter &painter, const AlertBar &alertBar, int yOffset);
   bool renderIcon(QPainter &painter, const QString &iconName, const QRectF &rect, const QColor &color);
-  // QRadialGradient createGlowGradient(const QRectF &rect, const QColor &color) const;
-  void drawGradientShadow(QPainter &painter, const QRectF &rect, const AlertProperties &properties);
+
+  // REMOVE - Shadow code
+  // void drawGradientShadow(QPainter &painter, const QRectF &rect, const AlertProperties &properties);
+
   void drawRoundedRect(QPainter &painter, const QRectF &rect, qreal xRadius, qreal yRadius);
 
   std::array<AlertBar, NUM_ALERT_BARS> alertBars;
