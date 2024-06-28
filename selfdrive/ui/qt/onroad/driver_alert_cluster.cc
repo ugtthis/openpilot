fe673cfb8d025ef0ae58d5cf9926aed96004410a
David Ratunuman <dratunuman@gmail.com>
2024-06-28 12:20:30 -0700

    UI device tweaks: font size, border width

--- selfdrive/ui/qt/onroad/driver_alert_cluster.cc
+++ selfdrive/ui/qt/onroad/driver_alert_cluster.cc
@@ -115,7 +115,7 @@ DriverAlertCluster::AlertProperties DriverAlertCluster::getAlertProperties(int a
   properties.fillColor = QColor(11, 16, 22);
   properties.iconColor = QColor(254, 255, 255);
   properties.textColor = QColor(254, 255, 255);
-  properties.borderWidth = 10;
+  properties.borderWidth = 18;
   properties.circleColors.fill(QColor(118, 117, 117));
 
   switch (alertLevel) {
--- selfdrive/ui/qt/onroad/driver_alert_cluster.h
+++ selfdrive/ui/qt/onroad/driver_alert_cluster.h
@@ -54,7 +54,7 @@ private:
   static constexpr int HORIZONTAL_PADDING = 20; // Controls horizontal space of the whole DAC UI from left edge of screen
   static constexpr int VERTICAL_PADDING = 20; // Controls vertical spacing between the DAC bars
   static constexpr int CIRCLE_SIZE = 33;
-  static constexpr int FONT_SIZE = 13;
+  static constexpr int FONT_SIZE = 24;
   static constexpr int ICON_SIZE = 50;
   static constexpr int CORNER_RADIUS = 41;
   static constexpr int CIRCLE_AREA_WIDTH = 310; // How close together circles are
