5085f93b8b70588d009c051a86a28f36ccdc135e
David Ratunuman <dratunuman@gmail.com>
2024-08-06 05:30:53 -0700

    Commented out all DMoji code that made it visible

--- selfdrive/ui/qt/onroad/annotated_camera.h
+++ selfdrive/ui/qt/onroad/annotated_camera.h
@@ -18,7 +18,8 @@ private:
 
   QVBoxLayout *main_layout;
   ExperimentalButton *experimental_btn;
-  QPixmap dm_img;
+  // REMOVE
+  // QPixmap dm_img;
   float speed;
   QString speedUnit;
   float setSpeed;
@@ -27,7 +28,8 @@ private:
   bool dmActive = false;
   bool hideBottomIcons = false;
   bool rightHandDM = false;
-  float dm_fade_state = 1.0;
+  // REMOVE
+  // float dm_fade_state = 1.0;
   bool v_ego_cluster_seen = false;
   int status = STATUS_DISENGAGED;
   std::unique_ptr<PubMaster> pm;
@@ -43,7 +45,8 @@ protected:
   void drawLaneLines(QPainter &painter, const UIState *s);
   void drawLead(QPainter &painter, const cereal::RadarState::LeadData::Reader &lead_data, const QPointF &vd);
   void drawHud(QPainter &p);
-  void drawDriverState(QPainter &painter, const UIState *s);
+  // REMOVE
+  // void drawDriverState(QPainter &painter, const UIState *s);
   inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
   inline QColor whiteColor(int alpha = 255) { return QColor(255, 255, 255, alpha); }
   inline QColor blackColor(int alpha = 255) { return QColor(0, 0, 0, alpha); }
